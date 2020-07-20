527bcca0ce76c3115793e75857fb3f23bb49f6ce
Daniel <zeroray@gmail.com>
2020-07-19 20:29:11 -0400

    ejercicios resueltos

---
+++ src/ejercicios-terminados/02-reduce.ts
@@ -0,0 +1,31 @@
+import { from } from 'rxjs';
+import { reduce, filter } from 'rxjs/operators';
+
+/**
+ * Ejercicio: 
+ * Sume todos los números del arreglo usando un reduce.
+ * Debe de filtrar para que sólo números sean procesados
+ * La salida debe de ser 32
+ * 
+ * Tip:
+ * isNan() es una función de JavaScript para determinar si es número
+ * Usar filter<any>(...) para no tener problemas de tipado.
+ */
+
+(() =>{
+
+
+  const datos = [1, 2, 'foo', 3, 5, 6, 'bar', 7, 8];
+
+  from(datos).pipe(
+    // Trabajar aquí
+    filter<any>( val => !isNaN( val ) ),
+    reduce( (acc, val) => acc + val )
+    
+  ).subscribe( console.log ) // La salida debe de ser 32
+
+
+
+})();
+
+		
